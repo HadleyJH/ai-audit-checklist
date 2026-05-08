fe2e07821ca66ceb76d96c3c95d18fcb3449f26f
HadleyJH <hadley.hodgson@summitmedia.com>
2026-05-08 09:48:15 +0100

    Create checklistData.js

---
+++ checklistData.js
@@ -0,0 +1,16 @@
+export const checklist = [
+  {
+    category: "Structured Data",
+    weight: 20,
+    questions: [
+      {
+        question: "Product schema implemented",
+        score: 10
+      },
+      {
+        question: "FAQ schema implemented",
+        score: 5
+      }
+    ]
+  }
+];
